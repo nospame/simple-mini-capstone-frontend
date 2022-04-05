fa939a54ebbdc2322f561de634a6270e7bdd760f
nospame <mapson.evan@gmail.com>
2022-04-05 12:02:26 -0700

    add axios, products index and products new actions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,62 @@
 <script>
+import axios from 'axios'
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      products: [],
+      newProductParams: {}
     };
   },
-  created: function () { },
-  methods: {}
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get('http://localhost:3000/products')
+        .then(
+          response => {
+            console.log(response.data);
+            this.products = response.data;
+          }
+        );
+    },
+    createProduct: function () {
+      axios.post('http://localhost:3000/products', this.newProductParams)
+        .then(
+          response => {
+            console.log(response);
+            this.newProductParams = {};
+            this.products.push(response.data)
+          },
+        );
+    }
+  }
 };
 </script>
 
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>{{ message }}</h1>Name:
+    <input v-model="newProductParams.name" />
+    <br />Description:
+    <input v-model="newProductParams.description" />
+    <br />Price:
+    <input v-model="newProductParams.price" />
+    <br />Image URL:
+    <input v-model="newProductParams.image_url" />
+    <br />
+    <button v-on:click="createProduct()">Create Product</button>
+    <div v-for="product in products">
+      {{ product.name }}, {{ product.formatted.price }}
+      <br />
+      <img v-bind:src="product.image_url" />
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+img {
+  width: 60%;
+}
+</style>
